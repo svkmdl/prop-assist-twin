c5006a9215ed8841fc183ab52b532210fb8d936f
svkmdl <souvikmondal15@gmail.com>
2026-04-25 12:18:31 +0200

    fix(log) : configured root logger level for lambda cloudwatch

--- backend/lambda_handler.py
+++ backend/lambda_handler.py
@@ -1,5 +1,9 @@
+import logging
 from mangum import Mangum
-from server import app
+from server import app, LOG_LEVEL
+
+# Ensure the Root Logger (which Lambda uses) matches your desired level
+logging.getLogger().setLevel(LOG_LEVEL)
 
 # Create the Lambda handler
 handler = Mangum(app)
